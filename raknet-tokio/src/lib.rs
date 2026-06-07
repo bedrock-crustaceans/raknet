8bbd9f7ecb202bdcb998ce21aa5ce04ca928d143
OmniacDev <64994204+OmniacDev@users.noreply.github.com>
2026-06-07 22:08:30 +1200

    feat: raknet-tokio prelude errors

--- raknet-tokio/src/lib.rs
+++ raknet-tokio/src/lib.rs
@@ -2,7 +2,7 @@ mod server;
 mod session;
 
 pub mod prelude {
-    pub use super::server::RakServer;
-    pub use super::session::RakSession;
-    pub use raknet::prelude::{RakPriority, RakReliability, RakServerConfig, RakSessionConfig};
+    pub use super::server::{RakServer, error::RakServerError};
+    pub use super::session::{RakSession};
+    pub use raknet::prelude::{RakPriority, RakReliability, RakServerConfig, RakSessionConfig, RakSessionError};
 }
