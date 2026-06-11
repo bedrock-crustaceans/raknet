6898cacd1e6ff03d02d4c81b1e2069bce9ee14be
OmniacDev <64994204+OmniacDev@users.noreply.github.com>
2026-06-11 23:37:34 +1200

    style: fmt

--- bevy-raknet/src/server.rs
+++ bevy-raknet/src/server.rs
@@ -1,12 +1,12 @@
+use bevy_app::prelude::*;
+use bevy_ecs::prelude::*;
+use raknet::prelude::RakServerInput::{SetMaxConnections, SetMessage};
+use raknet::prelude::{RakServer as RakServerIntl, RakSession as RakSessionIntl, *};
 use std::collections::HashMap;
 use std::io::ErrorKind;
 use std::net::{SocketAddr, UdpSocket};
-use std::time::{SystemTime};
-use bevy_app::prelude::*;
-use bevy_ecs::prelude::*;
+use std::time::SystemTime;
 use tracing::debug;
-use raknet::prelude::{*, RakServer as RakServerIntl, RakSession as RakSessionIntl};
-use raknet::prelude::RakServerInput::{SetMaxConnections, SetMessage};
 
 pub struct RakServerPlugin;
 
@@ -27,17 +27,17 @@ pub struct RakServer {
     intl: RakServerIntl,
     socket: UdpSocket,
     sessions: HashMap<RakSessionId, RakSessionIntl>,
-    buffer: Box<[u8]>
+    buffer: Box<[u8]>,
 }
 
 impl RakServer {
     pub fn new<T>(addr: SocketAddr, conf: T) -> Self
     where
-        T: FnOnce(&mut RakServerConfig), 
+        T: FnOnce(&mut RakServerConfig),
     {
         let mut config = RakServerConfig::default();
         conf(&mut config);
-        
+
         let socket = UdpSocket::bind(addr).unwrap();
 
         Self {
@@ -56,23 +56,27 @@ impl RakServer {
     }
 
     pub fn set_max_connections(&mut self, val: usize) {
-        let _= self.intl.handle(SetMaxConnections(val));
+        let _ = self.intl.handle(SetMaxConnections(val));
     }
-    
+
     fn update(&mut self) {
         loop {
             match self.socket.recv_from(&mut self.buffer) {
                 Ok((len, addr)) => {
-                    match self.intl.handle(RakServerInput::Datagram(self.buffer[..len].into(), addr, SystemTime::now()))  {
-                        Ok(_) => {},
-                        Err(e) => debug!("server failed to handle inbound datagram: {e}")
+                    match self.intl.handle(RakServerInput::Datagram(
+                        self.buffer[..len].into(),
+                        addr,
+                        SystemTime::now(),
+                    )) {
+                        Ok(_) => {}
+                        Err(e) => debug!("server failed to handle inbound datagram: {e}"),
                     }
                 }
                 Err(e) if e.kind() == ErrorKind::WouldBlock => break,
                 _ => {}
             }
         }
-        
+
         while let Some(output) = self.intl.poll() {
             match output {
                 RakServerOutput::SocketDatagram(buf, addr) => {
@@ -86,10 +90,10 @@ impl RakServer {
                     } else {
                         debug!("no session found with id {id:?}");
                     }
-                },
+                }
                 RakServerOutput::SessionConnected(session) => {
                     debug!("session {:?} connected", session.id);
-                    
+
                     self.sessions.insert(session.id, *session);
                 }
             }
